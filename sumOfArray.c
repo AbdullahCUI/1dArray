dd8bc91c55c0d052464c7177c265603ff7dbca9f
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-17 02:04:33 +0500

    correct error

--- sumOfArray.c
+++ sumOfArray.c
@@ -19,7 +19,7 @@ int main(){
    for(int i=0; i<5; i++){//content store in result array
     sumOfArray[i]=array[i]*array1[i];//i change the oparator addittion to multiplication now the array is multipling and store the result
 
-    printf("The sum of the index %d =%d\n", i,sumOfArray[i]);//print the result
+    printf("The multiplication of the index %d =%d\n", i,sumOfArray[i]);//print the result
    }
 
 
