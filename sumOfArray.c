178679e75ba623b405381c4bdd65733b04dba978
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-17 01:57:16 +0500

    sum of the array's

---
+++ sumOfArray.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+
+    int array[5];//first array
+
+    int array1[5];//second array
+
+   sumOfArray[5];//resutl'n array
+
+   for(int i=0; i<5; i++){ //first number index and storing content in first array
+    scanf("%d",&array[i]);
+   }
+   for(int i=0; i<5; i++){ //contnt store in second array
+    scanf("%d",&array1[i]);
+   }
+   for(int i=0; i<5; i++){//content store in result array
+    sumOfArray[i]=array[i]+array1[i];
+
+    printf("The sum of the index %d =%d\n", i,sumOfArray[i]);//print the result
+   }
+
+
+
+
+
+
+
+
+
+}
