dce8702afea01fb08962e9b07cbd4ad4766f99d3
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-17 22:46:29 +0500

    chaging in out for to program show more readable

--- putAndPrintOneDarrayContent.c
+++ putAndPrintOneDarrayContent.c
@@ -7,6 +7,6 @@ int main(void){
    	scanf("%d", &array[i]);//read and store the array content 
    }
    for(int i=0; i<5; i++){
-   	printf("the array content is %d\n ", array[i]);//printf all array content
+   	printf("\nIn the index %d value is %d ",i, array[i]);//printf all array content
    }
 }
